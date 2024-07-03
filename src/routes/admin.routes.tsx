a5145db0fd559cf1eb8b0833722e3a27f2905b88
sabrina <sabrinaahmednitu@gmail.com>
2024-07-03 23:10:51 +0600

    routes managed in a organized way

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,21 @@
+import { ReactNode } from 'react';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import CreateAdmin from '../pages/admin/CreateAdmin';
 import CreateFaculty from '../pages/admin/CreateFaculty';
 import CreateStudent from '../pages/admin/CreateStudent';
+import { NavLink } from 'react-router-dom';
 
-export const adminPaths2 = [
+type TRoute = {
+  path: string;
+  element: ReactNode;
+}
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
+};
+
+export const adminPaths = [
   {
     name: 'Dashboard',
     path: 'dashboard', // /admin e hit korle  e oi dashboard pabp
@@ -14,39 +26,89 @@ export const adminPaths2 = [
     children: [
       {
         name: 'Create Admin',
-        path: '/admin/create-admin',
-        label: <CreateAdmin></CreateAdmin>,
+        path: 'create-admin',
+        element: <CreateAdmin></CreateAdmin>,
       },
       {
         name: 'Create Faculty',
-        path: '/admin/create-faculty',
-        label: <CreateFaculty></CreateFaculty>,
+        path: 'create-faculty',
+        element: <CreateFaculty></CreateFaculty>,
       },
       {
         name: 'Create Student',
-        path: '/admin/create-student',
-        label: <CreateStudent></CreateStudent>,
+        path: 'create-student',
+        element: <CreateStudent></CreateStudent>,
       },
     ],
   },
-  
 ];
 
-export const adminPaths = [
-  {
-    path: 'dashboard', // /admin e hit korle  e oi dashboard pabp
-    element: <AdminDashboard></AdminDashboard>,
-  },
-  {
-    path: 'create-student', 
-    element: <CreateStudent></CreateStudent>,
-  },
-  {
-    path: 'create-admin', 
-    element: <CreateAdmin></CreateAdmin>,
-  },
-  {
-    path: 'create-faculty', 
-    element: <CreateFaculty></CreateFaculty>,
-  },
-];
+
+//MainLayout er items
+export const adminSidebarItems = adminPaths.reduce((acc: TSidebarItem[], item) => {
+  if (item.path && item.name) {
+    acc.push({
+      key: item.name,
+      label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+    });
+  }
+
+  if (item.children) {
+    acc.push({
+      key: item.name,
+      label: item.name,
+      //map array return kore //forEach kichu return korena
+      children: item.children.map((child) => ({
+        key: child.name,
+        label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+      })),
+    });
+  }
+
+  return acc;
+}, []);
+
+
+//routes.tsx er children
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if (item.children) {
+    //kono array dorkar ni tai forEach use korchi ,individual object lagbe sudhu
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+
+  return acc;
+}, []);
+
+
+
+//adminRoutes returns this arary
+// export const adminPaths = [
+//   {
+//     path: 'dashboard', // /admin e hit korle  e oi dashboard pabp
+//     element: <AdminDashboard></AdminDashboard>,
+//   },
+//   {
+//     path: 'create-student', 
+//     element: <CreateStudent></CreateStudent>,
+//   },
+//   {
+//     path: 'create-admin', 
+//     element: <CreateAdmin></CreateAdmin>,
+//   },
+//   {
+//     path: 'create-faculty', 
+//     element: <CreateFaculty></CreateFaculty>,
+//   },
+// ];
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,38 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import { adminPaths } from "./admin.routes";
+import { adminRoutes } from "./admin.routes";
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App></App>,
-    children: [
-      {
-        path: 'about',
-        element: <About></About>,
-      },
-    ],
   },
   {
     path: '/admin',
     element: <App></App>,
-        children: adminPaths
-    },
+    children: adminRoutes,
+  },
   {
     path: '/faculty',
     element: <App></App>,
-        children: adminPaths
-    },
+    children: adminRoutes,
+  },
   {
     path: '/student',
     element: <App></App>,
-        children: adminPaths
-    },
+    children: adminRoutes,
+  },
   //   {
+
+  //website 2 ta mone hobe tobe single application e
+
   //     path: '/admin',
   //     element:<AdminLayout></AdminLayout> ,
   //     children: [
